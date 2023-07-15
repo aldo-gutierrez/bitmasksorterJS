8949048ca8df1ea2c83df25c4b3b0a0d496c5027
aldo-gutierrez <aldo.agg@gmail.com>
2023-07-15 12:30:15 -0400

    some small refactors

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 import {arrayCopy} from "./sorter-utils.js";
 import {sortInt} from "./radix-bit-sorter-int.js";
 import {sortNumber} from "./radix-bit-sorter-number.js";
-import {pgCountSortInt} from "./pg-count-sorter-int.js";
+import {pCountSortInt} from "./p-count-sorter-int.js";
 
 export {
     arrayCopy,
     sortInt,
     sortNumber,
-    pgCountSortInt
+    pCountSortInt
 }
