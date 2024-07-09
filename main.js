d0ce5cfaa5de05c6db7e3f040c9c5b4ad41190c1
aldo-gutierrez <aldo.agg@gmail.com>
2024-07-08 22:06:41 -0400

    Some fixes

--- main.js
+++ main.js
@@ -2,12 +2,14 @@ import {arrayCopy} from "./sorter-utils.js";
 import {sortInt} from "./radix-bit-sorter-int.js";
 import {sortNumber} from "./radix-bit-sorter-number.js";
 import {pCountSortInt} from "./p-count-sorter-int.js";
-import {sortObjectInt} from "./radix-bit-sorter-object-int";
+import {sortObjectInt} from "./radix-bit-sorter-object-int.js";
+import {sortObjectNumber} from "./radix-bit-sorter-object-number.js";
 
 export {
     arrayCopy,
     sortInt,
     sortNumber,
     pCountSortInt,
-    sortObjectInt
+    sortObjectInt,
+    sortObjectNumber
 }
