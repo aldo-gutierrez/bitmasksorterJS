cfa235ed1d4da735ff49e1905d30163aa4c6c320
aldo-gutierrez <aldo.agg@gmail.com>
2024-07-24 00:49:17 -0400

    some fixes PCountSort

--- p-count-sorter-int.js
+++ p-count-sorter-int.js
@@ -19,7 +19,7 @@ export function pCountSortInt(array, start, endP1, bList, bListStart) {
         endP1 = array.length;
     }
     if (!bList) {
-        bList = getMaskAsArray( calculateMaskInt(array, start, endP1));
+        bList = getMaskAsArray(calculateMaskInt(array, start, endP1));
         bListStart = 0;
     }
     let bListNew = bList.slice(bListStart);
@@ -43,6 +43,7 @@ export function pCountSortInt(array, start, endP1, bList, bListStart) {
         pCountSortSections(array, start, endP1, sections);
     }
 }
+
 //
 // /**
 //  * Pigeonhole count sort is destructive count sort as it reconstructs (rebuilds)
@@ -54,67 +55,68 @@ export function pCountSortInt(array, start, endP1, bList, bListStart) {
 //  * when max-min (range > 2**25 is slower than javascript sorter)
 //  *    and when n (endP1-start) 2^17..2^20 (other ranges not tested yet)
 //  */
-// function pCountSortIntWithMinMax(array, start, endP1, min, max) {
-//     if (!start) {
-//         start = 0;
-//     }
-//     if (!endP1) {
-//         endP1 = array.length;
-//     }
-//     let n = endP1 - start;
-//     if (n < 2) {
-//         return;
-//     }
-//     if (!min || !max) {
-//         min = array[start];
-//         max = array[start];
-//         for (let i = start + 1; i < endP1; i++) {
-//             let value = array[i];
-//             if (value < min) {
-//                 min = value;
-//             }
-//             if (value > max) {
-//                 max = value;
-//             }
-//         }
-//     }
-//     let range = max - min + 1;
-//     if (range > 2 ** 24) {
-//         // if (!COUNT_SORT_ERROR_SHOWED) {
-//         //     console.error(COUNT_SORT_ERROR);
-//         //     COUNT_SORT_ERROR_SHOWED = true;
-//         // }
-//     }
-//     if (!Number.isInteger(range)) {
-//         console.error("Pigeonhole Count sort only works on integers")
-//         return;
-//     }
-//     let count = new Array(range).fill(0);
-//     for (let i = start; i < endP1; i++) {
-//         count[array[i] - min]++
-//     }
-//     let i = start;
-//     let j = min;
-//     for (; j <= max; j++) {
-//         let cMax = count[j - min];
-//         if (cMax > 0) {
-//             for (let c = 0; c < cMax; c++) {
-//                 array[i] = j;
-//                 i++;
-//             }
-//             if (i === endP1) {
-//                 break;
-//             }
-//         }
-//     }
-// }
-//
+export function pCountSortIntNoMask(array, start, endP1, min, max) {
+    if (!start) {
+        start = 0;
+    }
+    if (!endP1) {
+        endP1 = array.length;
+    }
+    let n = endP1 - start;
+    if (n < 2) {
+        return;
+    }
+    if (!min || !max) {
+        min = array[start];
+        max = array[start];
+        for (let i = start + 1; i < endP1; i++) {
+            let value = array[i];
+            if (value < min) {
+                min = value;
+            }
+            if (value > max) {
+                max = value;
+            }
+        }
+    }
+    let range = max - min + 1;
+    if (range > 2 ** 24) {
+        if (!COUNT_SORT_ERROR_SHOWED) {
+            console.error(COUNT_SORT_ERROR);
+            COUNT_SORT_ERROR_SHOWED = true;
+        }
+    }
+    if (!Number.isInteger(range)) {
+        console.error("Pigeonhole Count sort only works on integers")
+        return;
+    }
+    let count = new Array(range).fill(0);
+    for (let i = start; i < endP1; i++) {
+        count[array[i] - min]++
+    }
+    let i = start;
+    let j = min;
+    for (; j <= max; j++) {
+        let countJ = count[j - min];
+        if (countJ > 0) {
+            for (let c = 0; c < countJ; c++) {
+                array[i] = j;
+                i++;
+            }
+            if (i === endP1) {
+                break;
+            }
+        }
+    }
+}
+
+
 function pCountSortPositive(array, start, endP1, range) {
     if (range > (1 << 24)) {
-        // if (!COUNT_SORT_ERROR_SHOWED) {
-        //     console.error(COUNT_SORT_ERROR);
-        //     COUNT_SORT_ERROR_SHOWED = true;
-        // }
+        if (!COUNT_SORT_ERROR_SHOWED) {
+            console.error(COUNT_SORT_ERROR);
+            COUNT_SORT_ERROR_SHOWED = true;
+        }
     }
     let count = new Array(range).fill(0);
     for (let i = start; i < endP1; i++) {
@@ -123,9 +125,9 @@ function pCountSortPositive(array, start, endP1, range) {
     let i = start;
     let j = 0;
     for (; j <= count.length; j++) {
-        let cMax = count[j];
-        if (cMax > 0) {
-            for (let c = 0; c < cMax; c++) {
+        let countJ = count[j];
+        if (countJ > 0) {
+            for (let c = 0; c < countJ; c++) {
                 array[i] = j;
                 i++;
             }
@@ -179,7 +181,7 @@ function pCountSortSection(array, start, endP1, section) {
     let mask1 = getMaskRangeBits(getSectionStart(section), getSectionShift(section));
     for (let i = start; i < endP1; i++) {
         let element = array[i];
-        let key = (element & mask1) >> section.shift;
+        let key = (element & mask1) >> getSectionShift(section);
         count[key]++;
         number[key] = element;
     }
@@ -203,7 +205,8 @@ function pCountSortSection(array, start, endP1, section) {
 
 function pCountSortSections(array, start, endP1, sections) {
     let bits = 0;
-    for (let section in sections) {
+    for (let s = 0; s < sections.length; s++) {
+        let section = sections[s];
         bits += getSectionBits(section);
     }
     let range = 1 << bits;
@@ -242,7 +245,7 @@ function pCountSortSections(array, start, endP1, sections) {
 
 function getKeySN(element, sections) {
     let result = 0;
-    for (let i = 0; i < sections.length; i++) {
+    for (let i = sections.length - 1; i >= 0; i--) { //TODO CHECK THIS LINE, IN JAVA IMPLEMENTATION IS THE OPPOSIE DIRECTION
         let section = sections[i];
         let mask = getMaskRangeBits(getSectionStart(section), getSectionShift(section));
         let bits = (element & mask) >> getSectionShift(section);
